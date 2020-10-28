4e1bf1cb13d67a182654324698b3736c87ecd97a
Kim Yurim <kyl990312@kpu.ac.kr>
2020-10-29 02:48:07 +0900

    add project

---
+++ Sources/MyRiver.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MyState.h"
+#include "MyLog.h"
+class MyRiver : public MyState
+{
+private:
+	MyLog* logs[3];
+	const char* obj_path = "Resources/river.obj";
+	float speed = rand() % 20 + 10;
+public:
+	MyRiver();
+	void draw(glm::mat4 projection, glm::mat4 view, Shader shader);
+	void move();
+	bool check_removing();
+	void CreateLog(int idx);
+	void remove_log(int i);
+	~MyRiver();
+	float get_obs_speed(int idx);
+};
+
